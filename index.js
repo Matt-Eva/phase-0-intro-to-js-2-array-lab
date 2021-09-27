fabad3ebd223a3bf4ce841678bc9d8aa6b06ff1a
Matt Eva <matteweva@gmail.com>
2021-09-27 16:12:47 -0700

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = [`Milo`,`Otis`,`Garfield`];
+
+function destructivelyAppendCat(name){
+    cats.push(`${name}`);
+}
+function destructivelyPrependCat(name) {
+cats.unshift(`${name}`);
+}
+function destructivelyRemoveLastCat(){
+cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+cats.shift();
+}
+
+function appendCat(name) {
+    let oneMoreCatLast = [...cats, `${name}`];
+    return oneMoreCatLast;
+}
+
+function prependCat(name) {
+    let oneMoreCatFirst = [`${name}`, ...cats];
+    return oneMoreCatFirst;
+}
+
+function removeLastCat() {
+    let oneLessCatLast = [...cats.slice(0, cats.length - 1)];
+    return oneLessCatLast;
+}
+
+function removeFirstCat() {
+    let oneLessCatFirst = [...cats.slice(1)];
+    return oneLessCatFirst;
+}
+
+console.log(removeLastCat());
+
+console.log(removeFirstCat());
+
